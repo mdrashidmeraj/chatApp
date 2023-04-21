8667884f03080c90dd39846691483da4877bf576
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2023-04-22 02:07:23 +0530

    commit init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
 
 app.listen(port, () => {
     console.log('Server started on port 3000');
