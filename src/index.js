3637a21a1ca762c6f6e62289853c5dc7eba68c6b
Mohd Rashid Meraj <64818718+mdrashidmeraj@users.noreply.github.com>
2023-04-22 01:54:03 +0530

    commit init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(port, () => {
+    console.log('Server started on port 3000');
+    }
+);
